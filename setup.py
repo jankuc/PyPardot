09054c1c511d258dd779039387db934e01f1f5c7
Jan Kucera <jan.kucera@bighub.cz>
2025-09-25 11:11:23 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="PyPardot4",
-    version="1.1.17",
+    version="1.1.18",
     author="Matt Needham",
     author_email="matthew.m.needham@gmail.com",
 	license="MIT",
